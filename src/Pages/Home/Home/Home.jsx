a00e752a2f5d4e5eb5797723d8508476a7efe0d5
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-04-03 14:03:23 +0600

    initial setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">This is from home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+
+
+import {  NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    const navRoutes=(
+      <>
+        <li>
+            <NavLink to="/" className="nl ml-0 md:ml-3 font-serif font-bold">Home</NavLink>
+        </li>
+        <li>
+            <NavLink to="/about" className="nl ml-0 md:ml-10 font-serif font-bold" >About</NavLink>
+        </li>
+        <li>
+            <NavLink to="/services" className="nl ml-0 md:ml-10 font-serif font-bold" >Services</NavLink>
+        </li>
+        <li>
+            <NavLink to="/Gallary" className="nl ml-0 md:ml-10 font-serif font-bold" >Gallary</NavLink>
+        </li>
+        <li>
+            <NavLink to="/blog" className="nl ml-0 md:ml-10 font-serif font-bold" >Blog</NavLink>
+        </li>
+        <li>
+            <NavLink to="/contact" className="nl ml-0 md:ml-10 font-serif font-bold" >Contact</NavLink>
+        </li>
+    </>)
+    return (
+        <div className="navbar z-20 fixed bg-opacity-30 md:px-16">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul tabIndex={0} className="  dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52">
+            {navRoutes}
+            </ul>
+          </div>
+          <img src="Logo"
+          className="w-28 "></img>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <ul className=" menu-horizontal  ">
+            {navRoutes}
+          </ul>
+        </div>
+     
+      </div>
+    );
+};
+
+export default Navbar;
