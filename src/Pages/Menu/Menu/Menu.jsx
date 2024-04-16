2ba4c04a87d12af167bbac71782902061c3e681a
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-04-16 18:14:42 +0600

    menu page created

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,58 @@
-import { Helmet } from 'react-helmet-async';
-import Cover from '../../../Shared/Cover/Cover';
-import menuBg from "../../../assets/menu/banner3.jpg"
+import { Helmet } from "react-helmet-async";
+import Cover from "../../../Shared/Cover/Cover";
+import menuBg from "../../../assets/menu/banner3.jpg";
+import desertBg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaBg from "../../../assets/menu/pizza-bg.jpg";
+import saladBg from "../../../assets/menu/salad-bg.jpg";
+import soupBg from "../../../assets/menu/soup-bg.jpg";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "../MenuCategory/MenuCategory";
+
 const Menu = () => {
-    return (
-        <div>
-            <Helmet>FlavourVista | Our Menu</Helmet>
-            <Cover img={menuBg} title={'our menu'} details={'would you like to try dish?'}/>
-        </div>
-    );
+    const [menu]=useMenu();
+    const offered = menu.filter(item=>item.category === 'offered');
+    const dessert = menu.filter(item=>item.category === 'dessert');
+    const salad = menu.filter(item=>item.category === 'salad');
+    const soup = menu.filter(item=>item.category === 'soup');
+    const pizza = menu.filter(item=>item.category === 'pizza');
+   
+  return (
+    <div>
+      <Helmet>FlavourVista | Our Menu</Helmet>
+      <Cover
+        img={menuBg}
+        title={"our menu"}
+        details={"would you like to try dish?"}
+      />
+      <SectionTitle heading={"todays offer"} subHeading={"Dont miss"} />
+<MenuCategory items={offered}/>
+<Cover
+        img={desertBg}
+        title={"desert"}
+        details={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+      />
+<MenuCategory items={dessert}/>
+<Cover
+        img={pizzaBg}
+        title={"pizza"}
+        details={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+      />
+<MenuCategory items={pizza}/>
+<Cover
+        img={saladBg}
+        title={"salad"}
+        details={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+      />
+<MenuCategory items={salad}/>
+<Cover
+        img={soupBg}
+        title={"soup"}
+        details={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+      />
+<MenuCategory items={soup}/>
+    </div>
+  );
 };
 
 export default Menu;
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,17 @@
+import Button from "../../../Components/Button/Button";
+import MenuList from "../../Home/PopularMenu/MenuList";
 
 
-const MenuCategory = () => {
+const MenuCategory = ({items}) => {
     return (
-        <div>
-            
+      <div className="md:p-32 p-8">
+          <div className="grid md:grid-cols-2  gap-5 md:gap-0">
+        {
+             items.map((item,id)=><MenuList key={id} item={item}/>)
+         }
         </div>
+        <Button text={'Order Your Favourite Food'}/>
+      </div>
     );
 };
 
