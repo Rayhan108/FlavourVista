07b63261b2630a03be003fae6e552c5e5cd99b4c
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-08-16 20:06:28 +0600

    integrate react captha

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,71 +1,83 @@
 import { Link } from "react-router-dom";
 
-
-
 import { FcGoogle } from "react-icons/fc";
-
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { useEffect } from "react";
 const Login = () => {
-
-
-
-
-
+  
+  useEffect(()=>{
+  loadCaptchaEnginge(6); 
+  },[])
+  
   return (
-    <div className=' md:flex justify-center items-center min-h-screen  md:p-0 p-6'>
-      <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100  text-gray-900'>
-        <div className='mb-8 text-center'>
-          <h1 className='my-3 text-4xl font-bold'>Log In</h1>
-          <p className='text-sm text-amber-500 font-bold'>
+    <div className=" md:flex justify-center items-center min-h-screen  md:p-0 p-6">
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100  text-gray-900">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Log In</h1>
+          <p className="text-sm text-amber-500 font-bold">
             Sign in to access at FlavourVista
           </p>
         </div>
         <form
-          
-          noValidate=''
-          action=''
-          className='space-y-6 ng-untouched ng-pristine ng-valid'
+          noValidate=""
+          action=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
-          <div className='space-y-4'>
+          <div className="space-y-4">
             <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
+              <label htmlFor="email" className="block mb-2 text-sm">
                 Email address
               </label>
               <input
-                type='email'
-                name='email'
-                id='email'
+                type="email"
+                name="email"
+                id="email"
                 required
-                placeholder='Enter Your Email Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-emerald-400 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
+                placeholder="Enter Your Email Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-emerald-400 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
               />
             </div>
             <div>
-              <div className='flex justify-between'>
-                <label htmlFor='password' className='text-sm mb-2'>
+              <div className="flex justify-between">
+                <label htmlFor="password" className="text-sm mb-2">
                   Password
                 </label>
               </div>
               <input
-                type='password'
-                name='password'
-                autoComplete='current-password'
-                id='password'
+                type="password"
+                name="password"
+                autoComplete="current-password"
+                id="password"
                 required
-                placeholder='*******'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-emerald-400 bg-gray-200 text-gray-900'
+                placeholder="*******"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-emerald-400 bg-gray-200 text-gray-900"
+              />
+            </div>
+       
+            <div>
+              <div className="flex justify-between">
+              <LoadCanvasTemplate />
+              </div>
+              <input
+                type="text"
+                name="captha"
+                placeholder="Write the above text"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-emerald-400 bg-gray-200 text-gray-900"
               />
             </div>
           </div>
 
           <div>
             <button
-              type='submit'
-              className='bg-amber-500 w-full rounded-md py-3 text-white'
+              type="submit"
+              className="bg-amber-500 w-full rounded-md py-3 text-white"
             >
-              { (
-                'Continue'
-              )}
+              {"Continue"}
             </button>
           </div>
         </form>
@@ -74,26 +86,23 @@ const Login = () => {
             Forgot password?
           </button>
         </div> */}
-        <div className='flex items-center pt-4 space-x-1'>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-          <p className='px-3 text-sm dark:text-gray-400'>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+          <p className="px-3 text-sm dark:text-gray-400">
             Login with social accounts
           </p>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
-        <div
-       
-          className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
-        >
+        <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
         </div>
-        <p className='px-6 text-sm text-center text-gray-400'>
-          Don&apos;t have an account yet?{' '}
+        <p className="px-6 text-sm text-center text-gray-400">
+          Don&apos;t have an account yet?{" "}
           <Link
-            to='/signUp'
-            className='hover:underline hover:text-emerald-500 text-amber-500'
+            to="/signUp"
+            className="hover:underline hover:text-emerald-500 text-amber-500"
           >
             Sign up
           </Link>
@@ -101,7 +110,7 @@ const Login = () => {
         </p>
       </div>
     </div>
-  )
+  );
 };
 
 export default Login;
